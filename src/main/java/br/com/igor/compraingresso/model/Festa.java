b9008c18919c8f5ce3c39029755d1cfbdaa70094
Igor Santos <igorhnss@gmail.com>
2026-08-18 23:49:29 -0300

    feat: adiciona entidade JPA para ingresso e festa

--- src/main/java/br/com/igor/compraingresso/model/Festa.java
+++ src/main/java/br/com/igor/compraingresso/model/Festa.java
@@ -1,16 +1,28 @@
 package br.com.igor.compraingresso.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
 public class Festa {
 
+    @Id
+    @GeneratedValue
+    private int id;
     private String nome;
     private String horario;
     private String endereco;
+
+    public Festa() {}
 
     public Festa(String nome, String horario, String endereco) {
         this.nome = nome;
         this.horario = horario;
         this.endereco = endereco;
     }
+
+    public int getId() { return id; }
 
     public String getNome() {
         return nome;
--- src/main/java/br/com/igor/compraingresso/model/Ingresso.java
+++ src/main/java/br/com/igor/compraingresso/model/Ingresso.java
@@ -1,10 +1,21 @@
 package br.com.igor.compraingresso.model;
 
+import jakarta.persistence.*;
+
+@Entity
 public class Ingresso {
 
+    @Id
+    @GeneratedValue
+    private int id;
+
+    @ManyToOne
     private Festa festa;
+
     private Setor setor;
     private double preco;
+
+    public Ingresso(){}
 
     public Ingresso(Festa festa, Setor setor, double preco) {
         this.festa = festa;
@@ -16,11 +27,17 @@ public class Ingresso {
         return festa;
     }
 
+    public int getId() {
+        return id;
+    }
+
     public Setor getSetor() {
+
         return setor;
     }
 
     public double getPreco() {
+
         return preco;
     }
 
